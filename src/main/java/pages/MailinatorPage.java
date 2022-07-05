65f3440e104155a02159378eac53a8063a46ab1b
melnykd <d.melnykk@gmail.com>
2022-07-05 14:58:38 +0300

    paheObject HW

---
+++ src/main/java/pages/MailinatorPage.java
@@ -0,0 +1,69 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public abstract class MailinatorPage extends BasePage {
+
+    private By lastLetter = By.xpath("//td[contains(text(), 'qwerty')]");
+    private By inboxField = By.id("inbox_field");
+    private By goButton = By.className("primary-btn");
+    private By mailinatorSearchFieldLocator = By.xpath("//input[@id='search']");
+    private By mailinatorGoButtonLocator = By.xpath("//button[text()='GO']");
+    private By lastReceivedEmailLocator = By.xpath("//table[@class='table-striped jambo_table']//tbody//tr[1]");
+
+    public MailinatorPage(WebDriver driver) {
+        super(driver);
+        pageUrl = "https://www.mailinator.com/v4/public/inboxes.jsp";
+    }
+
+    public void clickLastLetter() {
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(lastLetter));
+        driver.findElement(lastLetter).click();
+    }
+
+    public void goToInbox(String inbox) {
+        driver.findElement(inboxField).sendKeys(inbox);
+        driver.findElement(goButton).click();
+    }
+
+    public void waitUntilLettersIsDisplayed() {
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i);
+            try {
+                if (driver.findElement(lastLetter).isDisplayed()) {
+                    return;
+                }
+            } catch (NoSuchElementException e) {
+                System.out.println(e.getMessage());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        throw new TimeoutException();
+    }
+
+    public void navigate() {
+        driver.get(pageUrl);
+
+    }
+
+    public void inputMailinatorEmail() {
+        driver.findElement(mailinatorSearchFieldLocator).sendKeys("Qwerty");
+    }
+
+    public void clickGoButton() {
+        driver.findElement(mailinatorGoButtonLocator).click();
+    }
+
+    public void clickOnLastReceivedLetter() {
+
+    }
+}
+
---
+++ src/test/java/ukrnet/UkrNetTest.java
@@ -0,0 +1,62 @@
+
+package ukrnet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.Loginpage;
+import pages.MailinatorPage;
+import testdata.User;
+
+public class UkrNetTest {
+
+    private WebDriver driver;
+    private String senderEmail = "testselenium1@ukr.net";
+    private String senderEmailPassword = "test1234";
+    private String receiverMailinatorEmail = "testselenium1@ukr.net";
+    private String mailText = "test body";
+    private String mailSubject = "test1234";
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("selenium.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void sendEmailToMailinatorTest() {
+        User user = new User(senderEmail, senderEmailPassword);
+        Loginpage loginPage = new Loginpage(driver) {
+            @Override
+            protected String pageUrl() {
+                return null;
+            }
+        };
+        loginPage.navigate();
+        loginPage.login(user);
+        HomePage homePage = new HomePage(driver);
+        homePage.waitUntilLoaded();
+        MailinatorPage mailinatorPage = new MailinatorPage(driver) {
+            @Override
+            protected String pageUrl() {
+                return null;
+            }
+        };
+
+        homePage.clickWriteLetter();
+        homePage.writeLetter(receiverMailinatorEmail, mailSubject, mailText);
+        homePage.sendLetter();
+        Assert.assertTrue(homePage.getTextLetterIsSend("Ваш лист надіслано"));
+        mailinatorPage.navigate();
+        mailinatorPage.goToInbox("testselenium1");
+        mailinatorPage.inputMailinatorEmail();
+        mailinatorPage.clickGoButton();
+        mailinatorPage.clickOnLastReceivedLetter();
+
+    }
+
+
+}
